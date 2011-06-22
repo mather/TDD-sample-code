6c34d69f4372badc50133c0df3cfd5d858a910de
mather <mather314@gmail.com>
2011-06-23 01:37:17 +0900

    Chap.8 テストからDollar.newを排除

--- money_spec.rb
+++ money_spec.rb
@@ -9,10 +9,10 @@ describe Dollar do
     end
 
     it "$5 x 2 = $10" do
-      @five.times(2).should == Dollar.new(10)
+      @five.times(2).should == Money.dollar(10)
     end
     it "$5 x 3 = $15" do
-      @five.times(3).should == Dollar.new(15)
+      @five.times(3).should == Money.dollar(15)
     end
   end
 end
@@ -33,9 +33,9 @@ describe Franc do
 end
 
 describe "等価性" do
-  it("$5 == $5") { Dollar.new(5).should == Dollar.new(5) }
-  it("$5 != $6") { Dollar.new(5).should_not == Dollar.new(6) }
+  it("$5 == $5") { Money.dollar(5).should == Money.dollar(5) }
+  it("$5 != $6") { Money.dollar(5).should_not == Money.dollar(6) }
   it("5 CHF == 5 CHF") { Franc.new(5).should == Franc.new(5) }
   it("5 CHF != 6 CHF") { Franc.new(5).should_not == Franc.new(6) }
-  it("$5 != 5 CHF") { Dollar.new(5).should_not == Franc.new(5) }
+  it("$5 != 5 CHF") { Money.dollar(5).should_not == Franc.new(5) }
 end

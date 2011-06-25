22fbd777d2817bc0a34327d5437735cdf3f83375
mather <mather314@gmail.com>
2011-06-25 13:20:16 +0900

    Chap.11 テストの重複を除去、項目の整理

--- money_spec.rb
+++ money_spec.rb
@@ -2,51 +2,29 @@
 
 require "./money.rb"
 
-describe "Dollar" do
-  context "積を計算する場合" do
-    before :all do
-      @five = Money.dollar(5)
+describe "積の計算" do
+  before :all do
+    @five = Money.dollar(5)
     end
 
-    it "$5 x 2 = $10" do
-      @five.times(2).should == Money.dollar(10)
-    end
-    it "$5 x 3 = $15" do
-      @five.times(3).should == Money.dollar(15)
-    end
+  it "$5 x 2 = $10" do
+    @five.times(2).should == Money.dollar(10)
   end
-end
-
-describe "Franc" do
-  context "積を計算する場合" do
-    before :all do
-      @five = Money.franc(5)
-    end
-
-    it "5 CHF x 2 = 10 CHF" do
-      @five.times(2).should == Money.franc(10)
-    end
-    it "5 CHF x 3 = 15 CHF" do
-      @five.times(3).should == Money.franc(15)
-    end
+  it "$5 x 3 = $15" do
+    @five.times(3).should == Money.dollar(15)
   end
 end
 
 describe "等価性" do
-  context "同じクラス" do
+  context "同じ通貨" do
     it("$5 == $5") { Money.dollar(5).should == Money.dollar(5) }
     it("$5 != $6") { Money.dollar(5).should_not == Money.dollar(6) }
-    it("5 CHF == 5 CHF") { Money.franc(5).should == Money.franc(5) }
-    it("5 CHF != 6 CHF") { Money.franc(5).should_not == Money.franc(6) }
   end
 
   context "異なる通貨" do
     it("$5 != 5 CHF") { Money.dollar(5).should_not == Money.franc(5) }
   end
-
 end
-
-
 
 describe "通貨" do
   it("Dollar's currency is 'USD'") { Money.dollar(1).currency.should == "USD" }
